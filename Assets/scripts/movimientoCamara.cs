c9c134eda5cde99ce209dd73b31e4d1efa5042d3
JBanuel <j.manuel.bs@hotmail.com>
2026-03-21 21:27:07 -0600

    add camara movement

---
+++ Assets/scripts/movimientoCamara.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class movimientoCamara : MonoBehaviour
+{
+    public Transform objetivo;
+    public float velocidadCamara = 0.025f;  
+    public Vector3 desplazamiento;
+    
+    [Header("Configuración de Bloqueo")]
+    public float alturaFija = 5f; 
+
+    private void LateUpdate()
+    {
+        if (objetivo == null) return;
+
+        Vector3 posicionDeseada = objetivo.position + desplazamiento;
+
+        posicionDeseada.y = alturaFija;
+
+        Vector3 posicionSuavisada = Vector3.Lerp(transform.position, posicionDeseada, velocidadCamara);
+        
+        transform.position = posicionSuavisada;
+    }
+}
